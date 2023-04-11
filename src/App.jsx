1c5faa2d500af8a451d7186b503360e310079a28
“lukenilsson” <luke.nilsson@gmail.com>
2023-04-11 15:09:37 -0400

    products new / css

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+function Header() {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <header>
+      <a href="#">Home</a> |<a href="#products-index"> All Products</a> |
+      <a href="#products-new"> New Products</a>
+      <h1>Nostalgia Toy Store</h1>
+    </header>
+  );
 }
 
-export default App
+function ProductsNew() {
+  return (
+    <div id="products-new">
+      <h2>New Product</h2>
+      <form>
+        <div>
+          Name:{" "}
+          <input type="text" id="Name" name="Name" placeholder="Willy Wonka" />
+        </div>
+        <div>
+          Price: <input type="text" id="Name" name="Name" placeholder="$$$" />
+        </div>
+        <div>
+          Description:{" "}
+          <input type="text" id="Name" name="Name" placeholder="Super Rad!" />
+        </div>
+      </form>
+    </div>
+  );
+}
+
+function ProductsIndex() {}
+
+function Footer() {}
+
+function Content() {
+  return (
+    <div>
+      <ProductsNew />
+      <ProductsIndex />
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
